418d6e9a0be954dcd9a296c9742c84ec0dae5902
mohamedelbayoumi <mohamedalbayoumi822@gmail.com>
2024-10-25 17:48:42 +0300

    setup the auth module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { mongooseModuleAsyncOptions } from './common/configs/mongoose.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRootAsync(mongooseModuleAsyncOptions)],
+  imports: [
+    MongooseModule.forRootAsync(mongooseModuleAsyncOptions),
+    AuthModule,
+  ],
 })
 export class AppModule {}
