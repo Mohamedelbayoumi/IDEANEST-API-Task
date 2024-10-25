4aafe849ab8738a38f12c1b269d3c12a855e5e84
mohamedelbayoumi <mohamedalbayoumi822@gmail.com>
2024-10-25 23:49:12 +0300

    adding organization model to organization setup

--- src/organizations/organizations.service.ts
+++ src/organizations/organizations.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Organization } from './schema/organization.schema';
 
 @Injectable()
-export class OrganizationsService {}
+export class OrganizationsService {
+  constructor(
+    @InjectModel(Organization.name)
+    private organizationModel: Model<Organization>,
+  ) {}
+}
