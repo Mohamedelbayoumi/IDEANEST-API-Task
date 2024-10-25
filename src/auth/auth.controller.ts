e921fb481279527b85db01abb96c36a78919eddc
mohamedelbayoumi <mohamedalbayoumi822@gmail.com>
2024-10-25 22:27:52 +0300

    feat: adding refresh token functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { SigninUserDto } from './dtos/login-user-dto';
+import { RefreshTokenDto } from './dtos/refresh-token-dto';
 
 @Controller()
 export class AuthController {
@@ -26,6 +27,17 @@ export class AuthController {
       message: 'User loggedin successfully',
       access_token,
       refresh_token,
+    };
+  }
+
+  @Post('refresh-token')
+  async rotateRefreshToken(@Body() refreshTokenDto: RefreshTokenDto) {
+    const { access_token, refresh_token } =
+      await this.authService.rotateRefreshToken(refreshTokenDto.refresh_token);
+    return {
+      message: 'refresh token rotation process is succeeded',
+      access_token,
+      refreshTokenDto,
     };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,6 +8,7 @@ import { ConfigService } from '@nestjs/config';
 
 import { UsersService } from '../users/users.service';
 import { BcryptjsService } from '../bcryptjs/bcryptjs.service';
+import { IPayload } from './interfaces/payload.interface';
 
 @Injectable()
 export class AuthService {
@@ -50,10 +51,34 @@ export class AuthService {
       sub: user.email,
     };
 
+    return await this.issueTokens(payload);
+  }
+
+  async rotateRefreshToken(refreshToken: string) {
+    const payload = await this.jwtService.verifyAsync(refreshToken, {
+      secret: this.refreshTokenSecret,
+    });
+
+    if (!payload) {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    const subPaylod = {
+      sub: payload.sub as string,
+    };
+
+    return await this.issueTokens(subPaylod);
+  }
+
+  private refreshTokenSecret: string = this.configService.get(
+    'REFRESH_TOKEN_SECRET_KEY',
+  );
+
+  private async issueTokens(payload: IPayload) {
     return {
       access_token: await this.jwtService.signAsync(payload),
       refresh_token: await this.jwtService.signAsync(payload, {
-        secret: this.configService.get('REFRESH_TOKEN_SECRET_KEY'),
+        secret: this.refreshTokenSecret,
         expiresIn: '2 days',
       }),
     };
